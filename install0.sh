2662007368003d08aaea1f218d66ed220694237f
Rujikorn <poringz@outlook.com>
2020-01-13 17:04:12 +0700

    add 10k theme

--- install0.sh
+++ install0.sh
@@ -7,4 +7,8 @@ chsh -s /usr/bin/zsh
 git clone https://github.com/supasorn/vim.git ~/.vim
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended"
 sh ~/.vim/install.sh
+
+# download 10k theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
 zsh
