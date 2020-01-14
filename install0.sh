cec923f3f7466040974705f89e12abc93cff98f0
Rujikorn Charakorn <omoplus_3@hotmail.com>
2020-01-15 03:04:16 +0700

    Update install0.sh

--- install0.sh
+++ install0.sh
@@ -5,13 +5,12 @@ sudo apt-get update
 sudo apt install neovim vim zsh tmux curl ripgrep -y
 chsh -s /usr/bin/zsh
 #git clone https://github.com/supasorn/vim.git ~/.vim
-# download 10k theme
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
 wget https://raw.githubusercontent.com/marciomazza/vim-brogrammer-theme/master/colors/brogrammer.vim ~/.vim/colors/brogrammer.vim
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended"
 sh ~/.vim/install.sh
-
-
+# download 10k theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
 zsh
