60bb3de37effee132c79bd85294085817715f943
Rujikorn Charakorn <omoplus_3@hotmail.com>
2020-01-15 02:57:26 +0700

    fix install0

--- install0.sh
+++ install0.sh
@@ -2,9 +2,9 @@
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/supasorn/vim/master/install0.sh)
 sudo add-apt-repository ppa:x4121/ripgrep
 sudo apt-get update
-sudo apt -y install neovim vim zsh tmux curl ripgrep
+sudo apt install neovim vim zsh tmux curl ripgrep -y
 chsh -s /usr/bin/zsh
-git clone https://github.com/supasorn/vim.git ~/.vim
+#git clone https://github.com/supasorn/vim.git ~/.vim
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended"
 sh ~/.vim/install.sh
 
