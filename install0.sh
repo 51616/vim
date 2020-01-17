c40770966e18b43853fae067efcd85eda02002c2
Rujikorn Charakorn <omoplus_3@hotmail.com>
2020-01-17 21:47:06 +0700

    add-apt package

--- install0.sh
+++ install0.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/supasorn/vim/master/install0.sh)
+sudo apt install software-properties-common
 sudo add-apt-repository ppa:x4121/ripgrep
 sudo apt-get update
 sudo apt install neovim vim zsh tmux curl ripgrep -y
-chsh -s /usr/bin/zsh
+
 #git clone https://github.com/supasorn/vim.git ~/.vim
 
 wget https://raw.githubusercontent.com/marciomazza/vim-brogrammer-theme/master/colors/brogrammer.vim ~/.vim/colors/brogrammer.vim
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended"
 sh ~/.vim/install.sh
+
+chsh -s /usr/bin/zsh
 
 zsh
 # download 10k theme
