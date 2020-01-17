35c83bd0acf19182a8632aeb748c28ac1f5a2306
Rujikorn Charakorn <omoplus_3@hotmail.com>
2020-01-17 23:02:47 +0700

    Update install0.sh

--- install0.sh
+++ install0.sh
@@ -12,6 +12,7 @@ sudo apt install neovim vim zsh tmux curl ripgrep -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended"
 sh ~/.vim/install.sh
 
+update-alternatives --install /usr/bin/python python /usr/bin/python3 10
 chsh -s /usr/bin/zsh
 # download 10k theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
