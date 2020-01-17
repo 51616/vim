fe50086889de180eb71fee24b323a00aceb62a33
Rujikorn Charakorn <omoplus_3@hotmail.com>
2020-01-17 22:28:43 +0700

    Update install0.sh

--- install0.sh
+++ install0.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/supasorn/vim/master/install0.sh)
-sudo apt install software-properties-common
-sudo add-apt-repository ppa:x4121/ripgrep
+
+sudo apt install software-properties-common -y
+# sudo add-apt-repository ppa:x4121/ripgrep
 sudo apt-get update
-sudo apt install neovim vim zsh tmux curl  -y
-#ripgrep
+sudo apt install neovim vim zsh tmux curl -y #ripgrep
 
 #git clone https://github.com/supasorn/vim.git ~/.vim
 #wget https://raw.githubusercontent.com/marciomazza/vim-brogrammer-theme/master/colors/brogrammer.vim ~/.vim/colors/brogrammer.vim
